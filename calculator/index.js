c046851b0383e78e05de5ca600c46c7df07cc012
bashermansour <basher@dafa.sa>
2022-08-03 12:55:03 +0300

    fix and format

--- calculator/index.js
+++ calculator/index.js
@@ -4,35 +4,23 @@ list_of_numbers.addEventListener("change", createListNumbers);
 const divide_by = document.getElementById("divide-by");
 divide_by.addEventListener("change", createListNumbers);
 
-
-
 function createListNumbers() {
+  const ul = document.getElementById("list-numbers");
+  ul.innerHTML = "";
 
-    const ul = document.getElementById("list-numbers");
-    ul.innerHTML = '';
+  var result_li = document.createElement("li");
 
-    var result_li = document.createElement('li');
+  result_li.setAttribute("class", "results");
+  result_li.innerHTML = "<h1> Results </h1>";
+  ul.appendChild(result_li);
 
-    result_li.setAttribute('class','results');
-    result_li.innerHTML = '<h1> Results </h1>'
-    ul.appendChild(result_li);
+  for (let i = 30; i <= list_of_numbers.value; i += 10) {
+    var li = document.createElement("li");
 
+    li.setAttribute("class", "item");
 
-    for(let i = 50; i <= list_of_numbers.value; i+=10){
+    ul.appendChild(li);
 
-        var li = document.createElement('li');
-
-        li.setAttribute('class','item');
-
-        ul.appendChild(li);
-
-        li.innerHTML = i + " = " + divide_by.value / i ;
-
-    }
-
+    li.innerHTML = i + " = " + divide_by.value / i;
+  }
 }
-
-
-
- 
-
--- calculator2/js/main.js
+++ calculator2/js/main.js
@@ -1,34 +1,21 @@
-(function($) {
+(function ($) {
+  "use strict";
 
-	"use strict";
-
-	$('[data-toggle="tooltip"]').tooltip()
-
+  $('[data-toggle="tooltip"]').tooltip();
 })(jQuery);
 
+var inputs = document.getElementsByClassName("form-control");
 
-
-
-var inputs = document.getElementsByClassName('form-control')
- 
 for (var i = 0; i < inputs.length; i++) {
-  
-	inputs[i].addEventListener('change', function(){
-		
-	var input_id = this.getAttribute('id');		
-	var input_items = document.getElementsByClassName(input_id);
+  inputs[i].addEventListener("change", function () {
+    var input_id = this.getAttribute("id");
+    var input_items = document.getElementsByClassName(input_id);
 
-	for (var i = 0; i < input_items.length; i++) {
-       var parent_id = input_items[i].parentElement.getAttribute('id');
-       console.log(input_items[i].parentElement.getAttribute('id'));
+    for (var i = 0; i < input_items.length; i++) {
+      var parent_id = input_items[i].parentElement.getAttribute("id");
+      console.log(input_items[i].parentElement.getAttribute("id"));
 
-	   input_items[i].innerHTML = this.value / parent_id ;
-
-	}
-
-
-	})
- }
-
-
- 
+      input_items[i].innerHTML = this.value / parent_id;
+    }
+  });
+}
